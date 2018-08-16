0f44962a2ae0bae916ff1fa638702d4abcd9e749
jh.park <ewlssss@naver.com>
2018-08-16 13:04:59 +0900

    branch2 추가

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
 class Test{
 	public static void main(String[] args){
 		system.out.println("Heo git");
-		ststem.out.println("Hi git");
+		system.out.println("Hi git");
+		system.out.println("branch2");
 	}
 }
 
